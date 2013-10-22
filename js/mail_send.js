37ae8efe5cb9273ab783698fbe8a6b53d1d3e11d
amir73911 <gafurovamir@gmail.com>
2013-10-22 23:47:10 +0400

    новые функции: показывать статус отправки, кнопка отправки избавился от хлама, исправил ошибки

--- js/mail_send.js
+++ js/mail_send.js
@@ -6,7 +6,7 @@
         // стандартные опции
         var options = $.extend( {
             mail_to : 'gafurovamir@gmail.com',      // адрес, на который отправлять
-            message_block : '.message',  // класс блока сообщений
+            show_message_block : true,  // показывать блока сообщений?
             send_button : '.send_button'   // кнопка отправки
         }, options);
 
@@ -14,23 +14,23 @@
         // кнопка отправки
         var button = form.find(options.send_button);
 
-        // если пользователь не указал где выводить сообщения, то создаем его с классом .message
-        if (form.find(options.message_block).length == 0){
-            form.append('<div class="'+options.message_block.replace(/\./g, "")+'"></div>');
+        // выводим или не выводим блок сообщений
+        if (options.show_message_block) {
+            form.append('<div class="message"></div>');
+
+            // блок для вывода сообщений
+            var message_block = form.find('.'+options.message_block);
         }
 
-        // блок для вывода сообщений
-        var message_block = form.find(options.message_block);
+        form.append('<input type="hidden" name="mail_to" value="'+options.mail_to+'"/>');
+
 
 
         // действия при клике
-        button.on("click", function(e){
+        button.click(function(e){
 
             e.preventDefault();
 
-            var name  = form.find(".name").val();
-            var email = form.find(".email").val();
-            var phone = form.find(".phone").val();
             var all_required = form.find(".required");
             var empty_fields = 0;
 
@@ -52,28 +52,25 @@
                 });
             });
 
+            var data = form.serialize();
+
             if (!empty_fields>0) {
                 $.ajax({
                     type: 'POST',
                     url: 'php/mail_send.php',
-                    data: {
-                        "mail_to": options.mail_to,
-                        "name": name,
-                        "phone": phone,
-                        "email": email
-                    },
-                    success: function(data) {
-                        if(data == "true") {
-                            message_block.removeClass('error').addClass('fine').html('Отправлено!').slideDown(100).delay(3000).slideUp(200);
+                    data: data,
+                    success: function(msg) {
+                        if(msg == "true") {
+                            if (options.show_message_block){message_block.removeClass('error').addClass('fine').html('Отправлено!').slideDown(100).delay(3000).slideUp(200)}
                             button.val("Отправлено!");
                         } else {
-                            message_block.removeClass('fine').addClass('error').html('Ошибка!').slideDown(100).delay(3000).slideUp(200);
+                            if (options.show_message_block){message_block.removeClass('fine').addClass('error').html('Ошибка!').slideDown(100).delay(3000).slideUp(200)}
                             button.val("Ошибка!");
                         }
                     }
                 });
             } else {
-                message_block.removeClass('fine').addClass('error').html('Вы не заполнили обязательные поля').slideDown(100).delay(3000).slideUp(200);
+                if (options.show_message_block){message_block.removeClass('fine').addClass('error').html('Вы не заполнили обязательные поля').slideDown(100).delay(3000).slideUp(200)}
                 button.val("Ошибка!");
             };
 
